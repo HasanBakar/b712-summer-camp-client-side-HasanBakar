84c41a3e1fd7da69c0560b6d8a2d8791e13b91f0
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-06-14 14:05:58 +0600

    animation added

--- src/Pages/Shared/Card/Card.jsx
+++ src/Pages/Shared/Card/Card.jsx
@@ -1,25 +1,28 @@
 /* eslint-disable react/prop-types */
+import { Slide } from "react-awesome-reveal";
 
 
 const Card = ({ pc }) => {
     const { image, name, price, seats_available, instructor } = pc;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img className="h-[300px]" src={image} alt="Dance class image" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Dance Type: {name}</h2>
-        <h3>Instructor: {instructor}</h3>
-        <p>Price: ${price}</p>
-        <p>Available seats: {seats_available}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-outline btn-secondary border-0 border-b-4">
-            select to Favorite
-          </button>
+    <Slide>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img className="h-[300px]" src={image} alt="Dance class image" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Dance Type: {name}</h2>
+          <h3>Instructor: {instructor}</h3>
+          <p>Price: ${price}</p>
+          <p>Available seats: {seats_available}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-outline btn-secondary border-0 border-b-4">
+              select to Favorite
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Slide>
   );
 };
 
--- src/Pages/Shared/InsCard/InsCard.jsx
+++ src/Pages/Shared/InsCard/InsCard.jsx
@@ -1,22 +1,24 @@
 /* eslint-disable no-unused-vars */
-
+import { Rotate } from "react-awesome-reveal";
 const insCard = ({ data }) => {
   const { name, image, email } = data;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure className="px-10 pt-10">
-        <img
-          src={image}
-          alt="Shoes"
-          className="w-20 h-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
-        />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">Name: {name}</h2>
-        <p>Email: {email} </p>
+    <Rotate>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img
+            src={image}
+            alt="Shoes"
+            className="w-20 h-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Name: {name}</h2>
+          <p>Email: {email} </p>
+        </div>
       </div>
-    </div>
+    </Rotate>
   );
 };
 
